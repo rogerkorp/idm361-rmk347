ef3b5dbd5d48ce7138948b50be7589b3b7a4123c
rogerkorp <rmk347@drexel.edu>
2022-10-24 22:54:07 -0400

    :sparkles: Pausing and Restarting the Timer!
    Created a new workflow that allows users to start and stop the timers.

--- week-06/scripts/start-timer.js
+++ week-06/scripts/start-timer.js
@@ -1,41 +1,90 @@
+
+let isPaused = false;
+let rewind;
+
 function startTimer(){
+
+    let userChosenMinutes = (document.getElementById('minutes').value) * 60000;
+    let userChosenSeconds = (document.getElementById('seconds').value) * 1000;
+    let timerAmount = userChosenMinutes + userChosenSeconds;
     let newTime = new Date().getTime();
-    let timerAmount = 30000;
+    
+/*     if (isPaused){
+        let pausedTime = new Date().getTime();
+        let x = setInterval(function() {
+            let currentTime = new Date().getTime();
+            let reverseTime = currentTime - pausedTime;
+            return reverseTime;
+            }, 1);
+    }; */
+    
     let oneHourFromNow = new Date(newTime + timerAmount).getTime();
+        
     let x = setInterval(function() {
-    let currentTime = new Date().getTime();
-    let timeDifference = oneHourFromNow - currentTime;
-    let minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        if(!isPaused){
 
-    let formattedMinutes = minutes.toLocaleString('en-US', {minimumIntegerDigits: 2});
-    let formattedSeconds = seconds.toLocaleString('en-US', {minimumIntegerDigits: 2});
+            console.log(newTime);
 
-    let timeRemaining = timeDifference/timerAmount;
-    
-    let greyPercentage = timeRemaining * 360;
-    let redPercentage = 360 - greyPercentage;
+            let currentTime = newTime += 4.55
+            
 
-    
-    console.log(redPercentage);
-    console.log(greyPercentage);
+            timeDifference = oneHourFromNow - currentTime;
+            let minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+            let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-    document.getElementById("countdown").innerHTML = formattedMinutes + ":" + formattedSeconds;
-    //document.getElementById('timer_ring').style.background = 'conic-gradient(red 48deg, grey 48deg 195deg)';
-    document.getElementById('timer_ring').style.background = 'conic-gradient(red ' + redPercentage + 'deg, lightgrey ' + redPercentage + 'deg ' + greyPercentage + 'deg)';
-    
-    document.getElementById('start_button').style.display = 'none';
-    
+            let formattedMinutes = minutes.toLocaleString('en-US', {minimumIntegerDigits: 2});
+            let formattedSeconds = seconds.toLocaleString('en-US', {minimumIntegerDigits: 2});
 
-    if (timeDifference < 0) {
-        clearInterval(x);
-        document.getElementById("countdown").innerHTML = "00:00";
-        document.getElementById('timer_ring').style.background = 'black';
-        document.getElementById('timer_center').style.background = 'red';
-        document.getElementById('countdown').style.color = 'white';
-        document.getElementById('body').style.background = 'red';
+            let timeRemaining = timeDifference/timerAmount;
+            let greyPercentage = timeRemaining * 360;
+            let redPercentage = 360 - greyPercentage;
 
-    }
+            document.getElementById("countdown").style.display = 'block';
+            document.getElementById("countdown").innerHTML = formattedMinutes + ":" + formattedSeconds;
+
+            // UNCOMMENT THE ABOVE LINE TO GET THE TICKING CLOCK BACK
+            document.getElementById('timer_ring').style.background = 'conic-gradient(var(--orange-red) ' + redPercentage + 'deg, lightgrey ' + redPercentage + 'deg ' + greyPercentage + 'deg)';
+            document.getElementById('user-input-timer').style.display = 'none';
+            document.getElementById('start_button').style.display = 'none';
+            document.getElementById('pause_button').style.display = 'block';
+            
+
+            if (timeDifference < 0) {
+                clearInterval(x);
+                document.getElementById("countdown").innerHTML = "00:00";
+                document.getElementById('timer_ring').style.background = 'black';
+                document.getElementById('timer_center').style.background = 'var(--orange-red)';
+                document.getElementById('countdown').style.color = 'white';
+                document.getElementById('body').style.background = 'var(--orange-red)';
+                document.getElementById('pause_button').style.display = 'none';
+                document.getElementById('reset_button').style.display = 'block';
+
+            }
+        };
     }, 1);
 
 };
+
+function pauseTimer(){
+    isPaused = true;
+    document.getElementById('pause_button').style.display = 'none';
+    document.getElementById('resume_button').style.display = 'block';
+};
+function resumeTimer(){
+    isPaused = false;
+    document.getElementById('pause_button').style.display = 'block';
+    document.getElementById('resume_button').style.display = 'none';
+}
+
+function resetTimer(){
+    timeDifference = 0;
+    document.getElementById("countdown").style.display = "none";
+    document.getElementById('timer_ring').style.background = 'lightgrey';
+    document.getElementById('timer_center').style.background = 'white';
+    document.getElementById('countdown').style.color = 'black';
+    document.getElementById('body').style.background = 'white';
+    document.getElementById('pause_button').style.display = 'none';
+    document.getElementById('reset_button').style.display = 'none';
+    document.getElementById('start_button').style.display = 'block';
+    document.getElementById('user-input-timer').style.display = 'flex';
+}
